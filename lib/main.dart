4534ab5a460a880b5f85ac8bb67df08c8679fa55
tomoki1590 <kaguratomato1@gmail.com>
2022-02-09 15:14:54 +0900

    riverpod準備、カレンダー日本語化

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/date_symbol_data_local.dart';
+
+import 'home_page.dart';
+
+void main() {
+  initializeDateFormatting("ja-JP").then((_) => runApp(ProviderScope(
+        child: MyApp(),
+      )));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'STVテックインターン',
+      home: HomePage(),
+    );
+  }
+}
